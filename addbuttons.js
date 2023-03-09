73ceab0982a2633b32771b73177de55e4cc9ea42
Edoardo Facchinelli <edoardo@fortnine.ca>
2023-03-09 12:12:38 -0500

    Add custom shortcuts, remove some superfluous html
    The superfluous underline issue still occurs

--- addbuttons.js
+++ addbuttons.js
@@ -6,23 +6,25 @@ function createElementFromHTML(htmlString) {
     return div.firstChild;
 }
 
-function generateTab(label, name, url, index=0)
+function generateTab(label, name, url, shortcut = '', index=0)
 {
+    const convertedName = name.replace(' ', '-');
+    // without the data-turbo-frame, the loads are no longer pjax and so a full page reload occurs
+    // the underline appears when the tab has `class=current` or `aria-current=page`
     return createElementFromHTML(
         `
 <li data-view-component="true" class="d-inline-flex">
     <a
-        id="${name}${gitHubButtonSuffix}"
+        id="${convertedName}${gitHubButtonSuffix}"
         href="${url}"
-        data-tab-item="i${index}${name}${gitHubButtonSuffix}"
+        data-tab-item="i${index}${convertedName}${gitHubButtonSuffix}"
         data-selected-links="repo_source repo_downloads repo_commits repo_releases repo_tags repo_branches repo_packages repo_deployments /mdn/webextensions-examples"
         data-pjax="#repo-content-pjax-container"
         data-turbo-frame="repo-content-turbo-frame"
-        data-hotkey="g c"
+        data-hotkey="${shortcut}"
         data-analytics-event="{&quot;category&quot;:&quot;Underline navbar&quot;,&quot;action&quot;:&quot;Click tab&quot;,&quot;label&quot;:&quot;${label}&quot;,&quot;target&quot;:&quot;UNDERLINE_NAV.TAB&quot;}"
-        aria-current="page"
         data-view-component="true"
-       class="UnderlineNav-item no-wrap js-responsive-underlinenav-item js-selected-navigation-item selected"
+       class="UnderlineNav-item no-wrap js-responsive-underlinenav-item js-selected-navigation-item"
     >
         <span data-content="${label}">${label}</span>
         <span id="${name}-repo-tab-count" data-pjax-replace="" data-turbo-replace="" title="Not available" data-view-component="true" class="Counter"></span>
@@ -32,10 +34,11 @@ function generateTab(label, name, url, index=0)
     );
 }
 
-function generateButton(label, name, url, index) {
+function generateButton(label, name, url, shortcut, index) {
+    const convertedName = name.replace(' ', '-');
     return createElementFromHTML(
         `
-<a href="${url}" class="btn btn-primary" role="button" data-hotkey="c" id="${name}${gitHubButtonSuffix}">
+<a href="${url}" class="btn btn-primary" role="button" data-hotkey="${shortcut}" id="${convertedName}${gitHubButtonSuffix}">
     <span class="d-none d-md-block">${label}</span>
     <span class="d-block d-md-none">${label}</span>
 </a>
@@ -48,16 +51,17 @@ function buildUrl(url)
     return window.location.pathname.replace(/(\/[^\/]+\/[^\/]+)\/?.*/g, "$1") + url;
 }
 
-function appendElement(nav, label, url, generatorFunction, name = undefined, index=0)
+function appendElement(nav, label, url, generatorFunction, name = undefined, index=0, shortcut='')
 {
     let codename = name;
     if (!name) {
         codename = label.toLowerCase();
     }
-    const exists = document.getElementById(`${codename}${gitHubButtonSuffix}`);
+    const convertedName = codename.replace(' ', '-');
+    const exists = document.getElementById(`${convertedName}${gitHubButtonSuffix}`);
     const fullUrl = buildUrl(url);
     if (!exists) {
-        nav[0].children[0].appendChild(generatorFunction(label, codename, fullUrl, index));
+        nav[0].children[0].appendChild(generatorFunction(label, codename, fullUrl, shortcut, index));
     }
 }
 
@@ -65,10 +69,10 @@ function addTabs()
 {
     let nav = document.getElementsByClassName("js-repo-nav");
     if (nav !== undefined) {
-        appendElement(nav, 'My branches', '/branches/yours', generateTab, undefined, 6);
-        appendElement(nav, 'All branches', '/branches/all', generateTab, undefined, 7);
-        appendElement(nav, 'Network', '/network', generateTab, undefined, 8);
-        appendElement(nav, 'New issue', '/issues/new/choose', generateButton, undefined, 9);
+        appendElement(nav, 'My branches', '/branches/yours', generateTab, undefined, 6, 'q m');
+        appendElement(nav, 'All branches', '/branches/all', generateTab, undefined, 7, 'q a');
+        appendElement(nav, 'Network', '/network', generateTab, undefined, 8, 'q n');
+        appendElement(nav, 'New issue', '/issues/new/choose', generateButton, undefined, 9, 'q c');
     }
 }
 
